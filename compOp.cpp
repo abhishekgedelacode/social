942beacf1b9b1ad12601a77ad9fce87bc0ea57dd
Abhishek Gedela <gabhishekji143@gmail.com>
2021-07-09 12:08:13 +0530

    modified the compop sourcecode,, addition of comments

--- compOp.cpp
+++ compOp.cpp
@@ -5,12 +5,18 @@ using namespace std;
 int main()
 {
     int a = 10, b = 5;
+    // > greaterthan
     cout << "(" << a << ">" << b << "): " << (a > b) << endl;
+    // >= greaterthan or equal
     cout << "(" << a << ">=" << b << "): " << (a >= b) << endl;
-    cout << "(" << a << "<" << b << "): " << (a < a) << endl;
-    cout << "(" << a << "<=" << b << "): " << (a <= a) << endl;
-    cout << "(" << a << "==" << b << "): " << (a == a) << endl;
-    cout << "(" << a << "!=" << b << "): " << (a != a) << endl;
+    // < lessthan
+    cout << "(" << a << "<" << a << "): " << (a < a) << endl;
+    // <= lessthan or equal
+    cout << "(" << a << "<=" << a << "): " << (a <= a) << endl;
+    // == equals to
+    cout << "(" << a << "==" << a << "): " << (a == a) << endl;
+    // != not equal to
+    cout << "(" << a << "!=" << a << "): " << (a != a) << endl;
 
     return 0;
 }
