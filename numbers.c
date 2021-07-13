e93d3c699edfceb5fe96c0a4908667295bc76b06
Abhishek Gedela <gabhishekji143@gmail.com>
2021-07-13 19:18:20 +0530

    added new program to find diff number sys values

---
+++ numbers.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int dec = 10, bin = 0b10, oct = 010, hex = 0x10;
+    printf("dec: %d\nbin: %d\n", dec, bin);
+    printf("oct: %d\nhex: %d\n", oct, hex);
+    return 0;
+}
