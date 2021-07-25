83d3a6b83822211b31a60b86bf14f9b7425fb3fb
Abhishek Gedela <gabhishekji143@gmail.com>
2021-07-25 11:32:49 +0530

    added new program on colors from fabulous lib installed from pip3

---
+++ colours.py
@@ -0,0 +1,10 @@
+from fabulous.color import bold, red, green, blue
+from fabulous.color import fg256, highlight256
+print("AbhiCode!")
+print(bold(red("AbhiCode!")))
+print(bold(green("AbhiCode!")))
+print(bold(blue("AbhiCode!")))
+print(bold(fg256('yellow', 'AbhiCode!')))
+print(highlight256('green', 'AbhiCode!'))
+print(bold(fg256('#55efc4', 'AbhiCode!')))
+print(highlight256('#0984e3', 'AbhiCode!'))
---
+++ fabtext.py
@@ -0,0 +1,6 @@
+# fabulous library
+# install using pip
+# pip install fabulous or pip3 install fabulous
+
+from fabulous import text
+print(text.Text("code", color='#0099ff', shadow=True, skew=5))
