84ec735de8a4ce8c4d50405e9da6b37090f09380
abhishekgedelacode <gabhishekji143@gmail.com>
2021-11-30 20:52:38 +0530

    completed aids a=day status

--- aidsDay.py
+++ aidsDay.py
@@ -1,5 +1,34 @@
-import pywhatkit as kt
-print("let's turn image to ascii art!!")
-sourcePath = "./photo.png"
-targetPath = "./asciiart"
-kt.image_to_ascii_art(sourcePath, targetPath)
+# import pywhatkit as kt
+# print("let's turn image to ascii art!!")
+# sourcePath = "./photo.png"
+# targetPath = "./asciiart"
+# kt.image_to_ascii_art(sourcePath, targetPath)
+from fabulous.color import bold, red, green, blue, white, yellow
+from time import sleep
+sleep(0.5)
+print(bold(red("\n\t\t\t Virus Detected !!!...")))
+sleep(0.5)
+print(bold(yellow("\t\t\t Be Aware ...")))
+sleep(0.5)
+print(bold(green("\t\t\t Bring Awareness ...")))
+sleep(0.5)
+print(bold(white("\t\t\t It's 1st Dec 2021")))
+sleep(1)
+print()
+print(bold(red("\t    S#&&&&&#   ")))
+print(bold(red("\t   #&&S  #&&#")))
+print(bold(red("\t  S&&    #&&#")))
+print(bold(red("\t  #&&S  S&&&S")))
+print(bold(red("\t   &&&##&&&#")))
+print(bold(red("\t   S&&&&&&#")))
+print(bold(red("\t    #&&&&# "), yellow("\t\t World AIDS Day")))
+print(bold(red("\t    S&&&&# "),   blue("\t\t      2021")))
+print(bold(red("\t   #&&&&&&&S")))
+print(bold(red("\t  #&&&#S&&&&S")))
+print(bold(red("\t #&&&#   #&&&S")))
+print(bold(red("\t#&&&#     #&&&S"), white(
+    "\tAn international day is dedicated to raising awareness")))
+print(bold(red("\tS&&#       #&&#"), white(
+    "\tof the AIDS pandemic caused by the spread of HIV infection")))
+print(bold(red("\t S#         ##*"),
+      white(" and mouring those who have died of the disease.")))
