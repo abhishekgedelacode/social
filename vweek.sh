fffe09cd089cd806634d20d1309424808d3e7847
abhishekgedelacode <gabhishekji143@gmail.com>
2022-02-12 21:57:03 +0530

    program for valentine week

---
+++ vweek.sh
@@ -0,0 +1,166 @@
+#! bin/bash
+RED="31"
+GREEN="32"
+YELLOW="33"
+BLUE="94"
+BOLDBLUE="\e[1;${BLUE}m"
+BOLDGREEN="\e[1;${GREEN}m"
+BOLDRED="\e[1;${RED}m"
+BOLDYELLOW="\e[1;${YELLOW}m"
+ITALICRED="\e[3;${RED}m"
+ENDCOLOR="\e[0m"
+echo -e  "\n\n\n\n\n\t\t\t\t💕 💞 💓 💗 💖 💘 💝"
+echo -e "\t\t\t\t❤️ ${BOLDRED}Valentine's Week❤️\n\t\t\t\t❤️ ${BOLDYELLOW} The Beginning  ❤️${ENDCOLOR}"
+echo -e "\t\t\t\t💝 💘 💖 💗 💓 💞 💕"
+####
+sleep 1
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..8}; do echo -n 🌷🌹; done
+echo -e "\n"
+figlet "    Rose Day" | lolcat
+echo -ne "\t"
+for i in {1..8}; do echo -n 🌷🌹; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..8}; do echo -n ❥☻💍☻❥; done
+echo -e "\n"
+figlet "    Propose Day" | lolcat
+echo -ne "\t"
+for i in {1..8}; do echo -n ❥☻💍☻❥; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..9}; do echo -n 🍭🍬🍫; done
+echo -e "\n"
+figlet "    Chocolate Day" | lolcat
+echo -ne "\t"
+for i in {1..9}; do echo -n 🍭🍬🍫; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..6}; do echo -n 🧸🐨🐻; done
+echo -e "\n"
+figlet "    Teddy Day" | lolcat
+echo -ne "\t"
+for i in {1..6}; do echo -n 🧸🐨🐻; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..8}; do echo -n 🎈🤝💌; done
+echo -e "\n"
+figlet "    Promise Day" | lolcat
+echo -ne "\t"
+for i in {1..8}; do echo -n 🎈🤝💌; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..7}; do echo -n 🫂 👫; done
+echo -e "\n"
+figlet "    Hug Day" | lolcat
+echo -ne "\t"
+for i in {1..7}; do echo -n 🫂 👫; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..2}; do echo -n 😘 👩‍❤️‍💋‍👨; done
+echo -e "\n"
+figlet "    Kiss Day" | lolcat
+echo -ne "\t"
+for i in {1..2}; do echo -n 😘 👩‍❤️‍💋‍👨; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+cat rough.txt | lolcat -a -d 5
+sleep 2
+clear 
+echo -e  "\n\n\n\n\n\t\t\t\t💕 💞 💓 💗 💖 💘 💝"
+echo -e "\t\t\t\t❤️ ${BOLDRED}Valentine's Week❤️\n\t\t\t\t❤️ ${BOLDYELLOW} The Conclusion ❤️${ENDCOLOR}"
+echo -e "\t\t\t\t💝 💘 💖 💗 💓 💞 💕"
+###
+sleep 1
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..8}; do echo -n 👋🤫; done
+echo -e "\n"
+figlet "    Slap Day" | lolcat
+echo -ne "\t"
+for i in {1..8}; do echo -n 👋🤫; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..6}; do echo -n 🦸 🦸; done
+echo -e "\n"
+figlet "    Kick Day" | lolcat
+echo -ne "\t"
+for i in {1..6}; do echo -n 🦸 🦸; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..13}; do echo -n 🍾🥰; done
+echo -e "\n"
+figlet "    Perfume Day" | lolcat
+echo -ne "\t"
+for i in {1..13}; do echo -n 🍾🥰; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..5}; do echo -n 🤷🙎‍🙎‍; done
+echo -e "\n"
+figlet "    Flirt Day" | lolcat
+echo -ne "\t"
+for i in {1..5}; do echo -n 🤷🙎‍🙎‍; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..15}; do echo -n 🧍‍🧍; done
+echo -e "\n"
+figlet "    Confession Day" | lolcat
+echo -ne "\t"
+for i in {1..15}; do echo -n 🧍‍🧍‍; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..8}; do echo -n 🤦 🤦; done
+echo -e "\n"
+figlet "    Missing Day" | lolcat
+echo -ne "\t"
+for i in {1..8}; do echo -n 🤦 🤦; done
+echo -e "\n"
+###
+sleep 0.5
+clear 
+echo -ne "\n\n\n\n\n\t"
+for i in {1..8}; do echo -n 💔 ❤️‍🩹; done
+echo -e "\n"
+figlet "    Break Up Day" | lolcat
+echo -ne "\t"
+for i in {1..8}; do echo -n 💔 ❤️‍🩹; done
+echo -e "\n"
+sleep 0.5 
+clear 
+cat rough.txt | lolcat 
