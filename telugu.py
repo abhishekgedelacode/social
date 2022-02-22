2768f84248c245b981cbd4bf4d2117abd0bfe628
abhishekgedelacode <gabhishekji143@gmail.com>
2022-02-22 19:26:55 +0530

    program for international mother language day

---
+++ telugu.py
@@ -0,0 +1,9 @@
+from fabulous.color import bold, yellow, red, green, blue
+from time import sleep
+print(bold(yellow("\n\t\t\t\t   అంతర్జాతీయ")))
+sleep(1)
+print(bold(red("\t\t\t\t మా తృభాషా దినోత్సవ")))
+sleep(1)
+print(bold(green("\t\t\t\t   శుభాకాంక్షలు")))
+sleep(2)
+print(bold(blue("\n\t\t\t\t౨౧ ఫిబ్రవరి ౨౦౨౨")))
