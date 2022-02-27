ca6a49da45a71dc809aec21685659514d5cef5ee
abhishekgedelacode <gabhishekji143@gmail.com>
2022-02-27 09:52:39 +0530

    program for national science day

---
+++ cvr.py
@@ -0,0 +1,55 @@
+from fabulous.color import bold, white, fg256
+from time import sleep
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t  |"))), sleep(0.08)
+print(bold(white("\t\t\t\t\t \\ /")), " monochromatic light beam"), sleep(0.08)
+print(bold(fg256("#95a5a6", "\t\t\t =================*=================")))
+print(bold(fg256("#bdc3c7", "\t\t\t                "), fg256(
+    "#FF0000", "/ "), fg256("#00FF00", "| "), fg256("#9400D3", "\\"), white(" Raman Effect")))
+
+print(bold("\n\t\t\t\t       ", fg256("#FF0000", "/"),
+      fg256("#FF7F00", "/"), fg256("#FFFF00", "/")), end="")
+print(bold(fg256("#00FF00", "|"), fg256("#0000FF", "\\"),
+      fg256("#4B0082", "\\"), fg256("#9400D3", "\\")))
+sleep(0.08)
+
+print(bold("\n\t\t\t\t     ", fg256("#FF0000", "//"),
+      fg256("#FF7F00", "/"), fg256("#FFFF00", "//")), end="")
+print(bold(fg256("#00FF00", "|"), fg256("#0000FF", "\\\\"),
+      fg256("#4B0082", "\\"), fg256("#9400D3", "\\\\")))
+sleep(0.08)
+
+print(bold("\n\t\t\t\t   ", fg256("#FF0000", "//"),
+      fg256("#FF7F00", "//"), fg256("#FFFF00", "//")), end="")
+print(bold(fg256("#00FF00", "|||"), fg256("#0000FF", "\\\\"),
+      fg256("#4B0082", "\\\\"), fg256("#9400D3", "\\\\")))
+sleep(0.08)
+
+print(bold("\n\t\t\t\t", fg256("#FF0000", "|||"),
+      fg256("#FF7F00", "|||"), fg256("#FFFF00", "|||")), end="")
+print(bold(fg256("#00FF00", "|||"), fg256("#0000FF", "|||"),
+      fg256("#4B0082", "|||"), fg256("#9400D3", "|||")))
+sleep(2)
+print(bold("\n\n\t\t\t\t", fg256("#FF0000", "Nat"),
+      fg256("#FF7F00", "ion"), fg256("#FFFF00", "al ")), end="")
+print(bold(fg256("#00FF00", "Sci"), fg256("#0000FF", "enc"),
+      fg256("#4B0082", "e D"), fg256("#9400D3", "ay!")))
+sleep(1)
+print(bold(white("\n\t Sir C.V.Raman was awarded with Nobel Prize in Physics in 1930.\n")))
---
+++ shiva.sh
@@ -0,0 +1,6 @@
+figlet "      Who"
+sleep 0.5 
+figlet "    Am  I  ?"
+sleep 1
+clear 
+echo -e "\n\n\n\t\t The Question is'nt Who is God ?,\n\t\t\t but What is God ?"
