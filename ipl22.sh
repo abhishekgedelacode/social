69593fe8d26dfe9881d2b9603b422cdb6f50384e
abhishekgedelacode <gabhishekji143@gmail.com>
2022-04-11 12:02:04 +0530

    modified fest file and new status program for ram navami

---
+++ ipl22.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+RED="31"
+GREEN="32"
+YELLOW="33"
+BLUE="94"
+BOLDBLUE="\e[1;${BLUE}m"
+BOLDRED="\e[1;${RED}m"
+BOLDGREEN="\e[1;${GREEN}m"
+BOLDYELLOW="\e[1;${YELLOW}m"
+ITALICRED="\e[3;${RED}m"
+ENDCOLOR="\e[0m"
+echo -e "${BOLDRED}"
+figlet "           I N D I A 's"
+sleep 0.5
+echo -e "${ENDCOLOR}"
+figlet "        B I G G E S T"
+sleep 0.5 
+echo -e "${BOLDGREEN}"
+figlet "    F E S T I V A L"
+sleep 2
+echo -e "${BOLDYELLOW}"
+for i in {1..15} 
+do 
+    clear
+    echo -e "\n\n\n\n\n\n\n\n"
+    figlet "                          ${i}"
+    sleep 0.1
+done 
+echo -e "${BOLDRED}"
+figlet "    E D I T I O N"
+figlet "        O F   I P L"
+sleep 1
+clear 
+echo -e "${BOLDYELLOW}\n\n\n\n\n"
+figlet "           9 + 1"
+echo -e "${BOLDGREEN}"
+figlet "    T E A M S"
+echo -e "${BOLDYELLOW}\n\n\t 1 HOST"
+echo -e "${BOLDGREEN}\n\t 9 GUESTS"
+sleep 2 
+clear
+echo -e "${BOLDBLUE}\n\n\t Mumbai Indians${BOLDYELLOW}"
+sleep 0.5
+echo -e "\t Chennai Super Kings\n\t Royal Challengers Bangole"
+echo -e "\t Kolkata Knight Riders\n\t Delhi Capitals"
+echo -e "\t Sunrisers Hyderabad\n\t Rajasthan Royals"
+echo -e "\t Punjab Kings\n\t Gujarat Titans\n\t Lucknow Super Gaints"
+sleep 1 
+clear 
+echo -e "${BOLDRED}\n\n\n\n\n\n"
+figlet "    W E L C O M E"
+sleep 0.5
+echo -e "${ENDCOLOR}"
+figlet "                         T O"
+sleep 0.5
+echo -e "${BOLDGREEN}"
+figlet "       M U M B A I"
+sleep 1 
+clear 
+echo -e "${ENDCOLOR}\n\n\n\n\n"
+figlet "      # IPLT20 s15"
+
+
+
+
+
---
+++ ram.py
@@ -0,0 +1,35 @@
+from time import sleep
+from fabulous.color import bold, yellow
+print("\n\n\n\n\n\n")
+print(bold(yellow("\t\t\tThe Greatest of All Time")))
+sleep(1)
+for i in range(10):
+    print()
+print("\t\t🏹")
+sleep(2)
+for i in range(25):
+    print()
+print("\t\t\t\t", end="")
+
+for i in range(10):
+    print("👿", end="")
+
+print("\n\n\n\n\n\t\t\t\t🏹")
+for i in range(25):
+    print()
+print("\t\t\t\t", end="")
+for i in range(10):
+    print("👿", end="")
+print("\n\t\t\t\t\t  /")
+print("\t\t\t\t\t /")
+print("\t\t\t\t\t/")
+print("\n\t\t\t\t    🏹")
+for i in range(10):
+    print()
+sleep(1)
+for i in range(25):
+    print()
+
+print(bold(yellow("\t\t\t\tJAI SRI RAM")))
+for i in range(10):
+    print()
