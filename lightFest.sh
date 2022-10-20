ff6604d65b2e1fa4948c6bef9f1c75debfbadf15
abhishekgedelacode <gabhishekji143@gmail.com>
2022-10-20 10:31:44 +0530

    diwali 2022

---
+++ lightFest.sh
@@ -0,0 +1,135 @@
+#!/bin/bash
+RED="31"
+GREEN="32"
+YELLOW="33"
+BLUE="94"
+BOLDBLUE="\e[1;${BLUE}m"
+BOLDRED="\e[1;${RED}m"
+BOLDGREEN="\e[1;${GREEN}m"
+BOLDYELLOW="\e[1;${YELLOW}m"
+ITALICRED="\e[3;${RED}m"
+ENDCOLOR="\e[0m"
+clear
+sleep 1
+######################
+for i in {1..20}
+do 
+    echo -e "\n"
+done
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t !"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\n\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\t\t\t\t !\n"
+sleep 0.5
+######################
+clear 
+for i in {1..10}
+do 
+    echo -e "\n"
+    done 
+
+echo -e "${BOLDYELLOW}\n\n\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t !"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\n\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\t\t\t\t !\n"
+sleep 0.5 
+#########################
+clear 
+for i in {1..5}
+do 
+    echo -e "\n"
+done
+
+echo -e "${BOLDYELLOW}\n\n\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t !"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\n\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\t\t\t\t !\n"
+sleep 0.5 
+######################
+clear 
+echo -e "${BOLDYELLOW}\n\n\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t !"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\n\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\t\t\t\t !\n"
+echo -e "${BOLDRED}\t\t\t\t\t\t !"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDBLUE}\t\t\t\t\t\t\t\t\t\t\t\t !"
+echo -e "${BOLDRED}\t\t\t !\n"
+sleep 0.5 
+##########################
+clear 
+echo -e "${BOLDYELLOW}\n\n\n\t\t\t\t\t\t\t\t\t\t *"
+echo -e "${BOLDRED}\t\t\t\t\t *"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t *"
+echo -e "${BOLDBLUE}\n\t\t\t\t\t\t\t\t *"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t\t\t\t *"
+echo -e "${BOLDBLUE}\t\t\t\t *\n"
+echo -e "${BOLDRED}\t\t\t\t\t\t *"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t\t *"
+echo -e "${BOLDBLUE}\t\t\t\t\t\t\t\t\t\t\t\t *"
+echo -e "${BOLDRED}\t\t\t *\n"
+sleep 0.5 
+############################
+clear 
+echo -e "${BOLDYELLOW}\n\n\t\t\t\t\t\t\t\t\\ ${BOLDBLUE}|${BOLDYELLOW} /"
+echo -e "${BOLDBLUE}\t\t\t\t\t\t\t\t-${BOLDRED} *${BOLDBLUE} -"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t\t/${BOLDBLUE} |${BOLDYELLOW} \\"
+
+echo -e "${BOLDRED}\n\n\t\t\t\t\t\\ ${BOLDYELLOW}|${BOLDRED} /"
+echo -e "${BOLDYELLOW}\t\t\t\t\t-${BOLDGREEN} *${BOLDYELLOW} -"
+echo -e "${BOLDRED}\t\t\t\t\t/${BOLDYELLOW} |${BOLDRED} \\"
+
+echo -e "${BOLDBLUE}\n\n\t\t\t\t\t\t\t\t\t\t\t\\ ${BOLDRED}|${BOLDBLUE} /"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t\t\t\t-${BOLDYELLOW} *${BOLDRED} -"
+echo -e "${BOLDBLUE}\t\t\t\t\t\t\t\t\t\t\t/${BOLDRED} |${BOLDBLUE} \\"
+
+echo -e "${BOLDYELLOW}\n\n\t\t\t\t\t\t\t\\ ${BOLDBLUE}|${BOLDYELLOW} /"
+echo -e "${BOLDBLUE}\t\t\t\t\t\t\t-${BOLDRED} *${BOLDBLUE} -"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t/${BOLDBLUE} |${BOLDYELLOW} \\"
+sleep 1
+##########################
+clear 
+echo -e "${BOLDRED}\n\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t      +"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t\t   .  |  ."
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t    \\   /"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t+  ${BOLDYELLOW}-  ${BOLDGREEN}*  ${BOLDYELLOW}-  ${BOLDRED}+"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t    /   \\"
+echo -e "${BOLDYELLOW}\t\t\t\t\t\t\t\t   .  |  ."
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t      +"
+sleep 1
+######################
+clear 
+echo -e "${BOLDGREEN}\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t\t\t      *"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t      +"
+echo -e "\t\t\t\t\t\t\t\t ${BOLDBLUE}* ${BOLDYELLOW}.  |  . ${BOLDBLUE}*"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t    \\   /"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t      ${BOLDGREEN}* ${BOLDRED}+  ${BOLDYELLOW}-  ${BOLDGREEN}*  ${BOLDYELLOW}-  ${BOLDRED}+ ${BOLDGREEN}*"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t    /   \\"
+echo -e "\t\t\t\t\t\t\t\t ${BOLDBLUE}* ${BOLDYELLOW}.  |  . ${BOLDBLUE}*"
+echo -e "${BOLDRED}\t\t\t\t\t\t\t\t      +"
+echo -e "${BOLDGREEN}\t\t\t\t\t\t\t\t      *\n"
+sleep 0.5
+clear 
+echo -e "${BOLDGREEN}\n\n\n\n\n\n\n\n\n\n\n\n\t\t\t\t\t\t      *"
+echo -e "${BOLDRED}\t\t\t\t\t\t      +"
+echo -e "\t\t\t\t\t\t ${BOLDBLUE}* ${BOLDYELLOW}.  |  . ${BOLDBLUE}*"
+echo -e "${BOLDRED}\t\t\t\t\t\t    \\   /"
+echo -e "${BOLDRED}\t\t\t\t\t      ${BOLDGREEN}* ${BOLDRED}+  ${BOLDYELLOW}-  ${BOLDGREEN}*  ${BOLDYELLOW}-  ${BOLDRED}+ ${BOLDGREEN}*"
+echo -e "${BOLDRED}\t\t\t\t\t\t    /   \\"
+echo -e "\t\t\t\t\t\t ${BOLDBLUE}* ${BOLDYELLOW}.  |  . ${BOLDBLUE}*"
+echo -e "${BOLDRED}\t\t\t\t\t\t      +"
+echo -e "${BOLDGREEN}\t\t\t\t\t\t      *\n"
+sleep 0.5
+echo -e "${BOLDRED}"
+figlet "                                  H A P P Y"
+echo -e "${BOLDYELLOW}"
+figlet "                             D I W A L I"
+echo -e "\n\n\n\n"
